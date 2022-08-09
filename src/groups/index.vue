2d96ddc508ec73e460684b881bf7a66b5e114a5d
tomcat <cooge123@gmail.com>
2022-08-09 10:52:26 +0800

    Update index.vue

--- src/groups/index.vue
+++ src/groups/index.vue
@@ -5,14 +5,13 @@
       <van-sidebar v-model="active" @change="onChange" >
         <van-sidebar-item v-for="group in state.groups" :title="group.categoryName" :badge="group.goodsList.length" v-show="group.goodsList.length>0" />
       </van-sidebar>
-
     </van-col>
     <van-col span="20" style="padding-left: 14px;">
       <van-cell-group :style='{
       "overflow-x":"scroll",
       "-webkit-overflow-scrolling": "touch",
-      "height": store.state.viewHeight+"px"
-    }' :border="false" ref="groupcell"  >
+      "height": (store.state.viewHeight-66)+"px"
+    }' :border="false" ref="groupCell"  >
         <div v-for="group in state.groups" :ref="setItemRef" class="group-views" v-show="group.goodsList.length>0" >
         <van-cell-group  :title="group.categoryName" class="my-van-cell-group"  >
           <van-cell  v-for="goods  in group.goodsList"  >
@@ -25,9 +24,14 @@
                 <van-tag plain type="danger">单位:{{goods.unit}}</van-tag>
               </template>
               <template #footer class="van-cell-group-footer">
-                <van-button round type="primary" @click="showOrder(goods)" size="normal">下单</van-button>
+                <van-stepper v-model="goods.goodsNum" default-value="0" min="0"  step="10" />
+                <van-cell-group style="margin-top: 15px;background: var(--van-card-background-color);">
+                  <van-button plain size="mini" @click="goods.goodsNum=50" type="primary" class="num-button">50</van-button>
+                  <van-button plain size="mini" @click="goods.goodsNum=100" type="primary" class="num-button">100</van-button>
+                  <van-button plain size="mini" @click="goods.goodsNum=200" type="primary" class="num-button">200</van-button>
+                  <van-button plain size="mini" @click="goods.goodsNum=300" type="primary" class="num-button">300</van-button>
+                </van-cell-group>
               </template>
-
             </van-card>
           </van-cell>
         </van-cell-group>
@@ -35,57 +39,31 @@
       </van-cell-group>
     </van-col>
   </van-row>
+    <van-cell-group class="van-cell-group-bottom">
+      <van-cell :title="'总数:'+order.goodsNum"  :label="'种类:'+order.orderCategoryNum"  >
 
-    <van-action-sheet v-model:show="state.show" title="选择数量" >
-      <div class="content">
-
-        <van-card
-            :desc="state.goods.remark"
-            :title="state.goods.goodsName"
-            :thumb="imageUrl+state.goods.imageLink"
-        >
-          <template #tags>
-            <van-tag plain type="danger">单位:{{state.goods.unit}}</van-tag>
-          </template>
-        </van-card>
-        <van-cell-group inset>
-          <van-cell title="输入数量" >
-            <template #value>
-              <van-stepper v-model="state.formData.goodsNum"  />
-            </template>
-          </van-cell>
-        </van-cell-group >
-        <van-divider />
-        <van-cell-group  title="滑动调整数量">
-        <van-slider v-model="state.formData.goodsNum"  :max="200" >
-          <template #button>
-            <div class="custom-button">{{ state.formData.goodsNum }}</div>
-          </template>
-        </van-slider>
-        </van-cell-group>
-        <van-divider />
-        <van-button type="primary" @click="subscribe" block>确认下单</van-button>
-      </div>
-    </van-action-sheet>
-
+        <template #value>
+          <van-button type="success">下单</van-button>
+        </template>
+      </van-cell>
+    </van-cell-group>
   </van-config-provider>
 </template>
 
 <script>
-import {ref,onMounted,onBeforeUpdate,onUpdated,reactive,nextTick} from "vue";
+import {ref, onMounted, onBeforeUpdate, onUpdated, reactive, nextTick, computed} from "vue";
 import {Toast,Dialog} from "vant";
 import {scan,orderGoods} from '@/api/goods'
 import {useStore} from 'vuex';
 export default {
   setup() {
     const active  = ref(0);
-    const groupcell = ref(null);
+    const groupCell = ref(null);
 
     const store = useStore()
     const vanConfigProvider = ref(null);
     const imageUrl = ref(import.meta.env.VITE_APP_IMAGE_API)
-    const state = reactive({goods:{},num:0,groups:[],show:false,formData:{goodsNum:50,goodsId:0}})
-    // const groups =
+    const state = reactive({groups:[]})
     let groupItemRels =[]
     const setItemRef=(el)=>{
       if(el){
@@ -99,6 +77,21 @@ export default {
         }))
 
     })
+
+    const order = computed(()=>{
+      let goodsNum = 0
+      let orderCategoryNum = 0
+      for(const group of state.groups){
+        for(const goods of group.goodsList){
+          if(goods.goodsNum){
+            goodsNum = goods.goodsNum+goodsNum
+            orderCategoryNum = orderCategoryNum+1
+          }
+        }
+      }
+      return {goodsNum,orderCategoryNum}
+    })
+
     const onChange = (index)=> {
       Toast({message:state.groups[index].categoryName,position: 'bottom'})
       let scrollHeight = 0
@@ -106,46 +99,18 @@ export default {
       {
         scrollHeight = scrollHeight+groupItemRels[i].offsetHeight
       }
-      groupcell.value.$el.scrollTop = scrollHeight
+      groupCell.value.$el.scrollTop = scrollHeight
     };
-    const showOrder=(goods)=>{
-      state.formData.goodsId = goods.goodsId
-      state.goods = goods
-      state.show = true
-    }
-    const subscribe=()=>{
-      Dialog.confirm({
-        title: '添加货物',
-        message:
-            '确定添加 '+state.goods.goodsName+':'+state.formData.goodsNum+state.goods.unit+'吗。',
-      })
-          .then(() => {
-            orderGoods(state.formData).then((response=>{
-              if(response.data.responseBody){
-                Toast({message:"添加成功",position: 'bottom'})
-                state.show = false
-              }
-            }))
-          })
-          .catch(() => {
-            // on cancel
-          });
-    }
     const themeVars = {cellGroupTitleColor:'var(--van-primary-color)'};
-
     return {
-      active,onChange,state,setItemRef,groupcell,themeVars,showOrder,vanConfigProvider,subscribe,imageUrl,store
+      active,onChange,state,setItemRef,groupCell,themeVars,vanConfigProvider,imageUrl,store,order
     }
-
   }}
-
-
 </script>
 <style scoped>
 .van-row {
   display: flex;
   flex-wrap: wrap;
-  position: fixed;
   width: 100%;
 }
 .van-config-provider{
@@ -164,5 +129,8 @@ export default {
   text-align: center;
   background-color: #ee0a24;
   border-radius: 100px;
+}
+.van-cell-group-bottom{
+
 }
 </style>
