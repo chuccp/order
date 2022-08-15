269b5b81809d866c5e05fe0ed8b4ffed73b4f939
tomcat <cooge123@gmail.com>
2022-08-15 13:21:20 +0800

    Update permission.js

--- src/util/permission.js
+++ src/util/permission.js
@@ -2,10 +2,12 @@ import store from "../store";
 
 export const hasRole = function () {
     const roles = store.state.user.roles
-    for (const index in arguments) {
-        const userRole = arguments[index]
-        if (roles.some((r) => r === userRole)) {
-            return true
+    if(roles){
+        for (const index in arguments) {
+            const userRole = arguments[index]
+            if (roles.some && roles.some((r) => r === userRole)) {
+                return true
+            }
         }
     }
     return false
