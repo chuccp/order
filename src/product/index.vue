44b931830f90920cf286e587dd9a45b06ec81ec6
tomcat <cooge123@gmail.com>
2022-08-22 21:22:44 +0800

    Update index.vue

--- src/product/index.vue
+++ src/product/index.vue
@@ -25,7 +25,7 @@
 
     <van-cell title="图片上传" class="show-file">
       <template #extra>
-        <van-uploader :preview-options="{closeable: true}" v-model="fileList" upload-text="建议上传正方形80*80图片" name="file" accept="image/png, image/jpeg" :after-read="afterRead" max-count="1"  />
+        <van-uploader :preview-options="{closeable: true}" v-model="fileList" upload-text="建议上传正方形图片" name="file" accept="image/png, image/jpeg" :after-read="afterRead" max-count="1"  />
       </template>
     </van-cell>
 
